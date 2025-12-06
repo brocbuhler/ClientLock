ba146515c59b1a613f76849b112e34019c3ea945
Broc Buhler <brocbuhler@gmail.com>
2025-12-06 11:33:27 -0800

    basic navigation added

---
+++ client/app/agent/view/page.js
@@ -0,0 +1,10 @@
+
+export default function Agents() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      <main>
+        This is the Agents page
+      </main>
+    </div>
+  );
+}
