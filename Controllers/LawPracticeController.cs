6008bd5c335e5ac72ec121dde338cb7191c91a3e
Broc Buhler <brocbuhler@gmail.com>
2025-12-15 20:35:35 -0600

    Law practice controller added and tested

---
+++ Controllers/LawPracticeController.cs
@@ -0,0 +1,52 @@
+using ClientLock.models;
+using ClientLock.Data;
+using ClientLock.models.DTO;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.EntityFrameworkCore;
+
+namespace ClientLock.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+
+public class LawController : ControllerBase
+{
+    private ClientLockDbContext _dbContext;
+
+    public LawController(ClientLockDbContext context)
+    {
+        _dbContext = context;
+    }
+
+  [HttpGet]
+  [Authorize]
+    public IActionResult Get()
+    {
+        return Ok(_dbContext.LawPractices.Select(c => new LawPracticeDTO {Type = c.Type, Id = c.Id, Description = c.Description}).ToList());
+    }
+  [HttpGet("{lawpracticeid}")]
+  [Authorize]
+  public IActionResult GetAgentByLaw( int lawpracticeid)
+    {
+        return Ok(_dbContext.AgentLawPractices
+        .Where(lp => lp.LawPracticeId == lawpracticeid)
+        .Include(lp => lp.Agent)
+        .Select(lp => new AgentLawPracticeDTO
+        {
+            Id = lp.Id,
+            LawPracticeId = lp.LawPracticeId,
+            AgentId = lp.AgentId,
+            Agent = new AgentDTO
+            {
+                Id = lp.Agent.Id,
+                FirstName = lp.Agent.FirstName,
+                LastName = lp.Agent.LastName,
+                UserProfileId = lp.Agent.UserProfileId,
+                Phone = lp.Agent.Phone,
+                Email = lp.Agent.Email,
+                Image = lp.Agent.Image
+            }
+        }).ToList());
+    }
+}
--- models/AgentLawPractice.cs
+++ models/AgentLawPractice.cs
@@ -5,6 +5,7 @@ public class AgentLawPractice
     public int Id { get; set; }
     [Required]
     public int AgentId { get; set; }
+    public Agent Agent { get; set; }
     [Required]
     public int LawPracticeId { get; set; }
 }
